b47c9f4999dadba0252dfab8ee7c834285e01d67
justapea25 <s3926201@rmit.edu.vn>
2021-12-28 22:23:27 +0700

    Update f1_list_all_items.py

--- f1_list_all_items.py
+++ f1_list_all_items.py
@@ -1,5 +1,10 @@
 # Feature 1: List all items:
+"""
+This function will
+: param
+: return
+"""
 def list_all_books(books_list):
     print("All books available:")
     for book in books_list:
-        print("{0:>2} {1} {2}".format(book["id"], "|", book["name"]))
+        print("{0:>2} {1} {2}".format(book["id"], "|", book["name"]))   # Format string to align the book list perfectly
